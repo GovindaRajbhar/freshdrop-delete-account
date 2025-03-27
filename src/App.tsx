60c5fe78e27ce1e6d53da056d8ddc94eedd20517
GovindaRajbhar <rajbhar1993govinda@gmail.com>
2025-03-27 19:10:52 +0530

    code clean up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import DeleteAccount from "./pages/DeleteAccount";
 import "./App.css"; // Ensure you have CSS for styling
 
